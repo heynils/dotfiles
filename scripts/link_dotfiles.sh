17d84c518ea5a27dc97a3a8d786d21498c215ac5
heynils <nils.heyman@gmail.com>
2022-10-17 21:26:53 +0200

    Add nvim dotfile and add to link script

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
-echo "Dotfiles symlinked, restarting zsh"
+
+echo "Dotfiles symlinked, restarting zsh..."
 source ~/.zshrc
