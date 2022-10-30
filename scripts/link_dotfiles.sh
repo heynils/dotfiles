392ab3a45e78b871915c891899c1bdf2990ee4e5
heynils <nils.heyman@gmail.com>
2022-10-30 12:47:58 +0100

    link nvim to right path in script

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -2,7 +2,7 @@
 
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/nvim/ ~/.config/
 
 
 echo "Dotfiles symlinked, restarting zsh..."
