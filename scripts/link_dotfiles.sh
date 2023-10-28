0f5bc41b521a20a34bc82e5b16ce563411acd4ee
Nils Heyman <nils.heyman@omegapoint.se>
2023-10-28 10:42:15 +0200

    wsl.conf link

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/nvim/ ~/.config/
 ln -s ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
+sudo ln -s ~/dotfiles/wsl/wsl.conf /etc/wsl.conf
 
 echo "Dotfiles symlinked, restarting zsh..."
 source ~/.zshrc
