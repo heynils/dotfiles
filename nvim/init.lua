517c60920b3e2caad6f9f5c3f4e93052cdc27f31
Nils Heyman <nils.heyman@omegapoint.se>
2023-11-10 16:10:01 +0100

    add modified nvchad nvim setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,22 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.telescope"
-require "user.lsp"
-require "user.cmp"
--- require "user.treesitter"
-require "user.nvim-tree"
-require "user.autopairs"
-require "user.comment"
-require "user.indentline"
-require "user.colorizer"
-require "user.colorscheme"
--- require "statusline"
-require("kanagawa").setup()
-require("gitsigns").setup()
--- pcall(vim.cmd, "colorscheme " .. "kanagawa")
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
+OmniSharp_highlighting = 0
