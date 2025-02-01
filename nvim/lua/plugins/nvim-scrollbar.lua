877507a0f5fcd1dc8f62cff6ea7dd7f537d06380
Nils Heyman <nils.heyman@gmail.com>
2025-02-01 19:23:21 +0100

    fix nvim scroll bar setup

--- nvim/lua/plugins/nvim-scrollbar.lua
+++ nvim/lua/plugins/nvim-scrollbar.lua
@@ -2,13 +2,14 @@ return {
     {
         "petertriho/nvim-scrollbar",
         config = function()
-            require("scrollbar").setup()
-            handlers = {
-                cursor = false,
-                diagnostic = true,
-                handle = true,
-                gitsigns = true,
-            }
+            require("scrollbar").setup({
+                handlers = {
+                    cursor = false,
+                    diagnostic = true,
+                    handle = true,
+                    gitsigns = true,
+                }
+            })
         end,
         lazy = false,
     }
