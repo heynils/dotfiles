73d166c2ed4fe6df579e48895ceefdff1764cecd
Nils Heyman <nils.heyman@gmail.com>
2025-02-23 14:29:59 +0100

    show relative path in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
                 sections = {
                     lualine_a = { 'mode' },
                     lualine_b = { 'branch', 'diff', 'diagnostics' },
-                    lualine_c = { 'filename' },
+                    lualine_c = { { 'filename', path = 3, } },
                     lualine_x = {
                         {
                             function()
