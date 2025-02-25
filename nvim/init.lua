61eab15db25209975b1f55a406509b1a523c8000
Nils Heyman <nils.heyman@gmail.com>
2025-02-25 17:53:17 +0100

    remove commented line

--- nvim/init.lua
+++ nvim/init.lua
@@ -202,7 +202,6 @@ end
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     if vim.fn.argc() == 0 then
-      -- require("nvim-tree.api").tree.open()
       require("telescope.builtin").oldfiles()
     end
   end,
