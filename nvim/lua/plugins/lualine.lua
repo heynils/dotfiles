7eeec58e1851dc1e8e675f7099baef4d15c883e9
Nils Heyman <nils.heyman@gmail.com>
2025-04-19 19:32:14 +0200

    edit

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
                 sections = {
                     lualine_a = { 'mode' },
                     lualine_b = { 'branch', 'diff', 'diagnostics' },
-                    lualine_c = { { 'filename', path = 1, } },
+                    lualine_c = { { 'filename', path = 2, } },
                     lualine_x = {
                         {
                             function()
