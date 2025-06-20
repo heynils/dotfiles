f11186b308f316bf3df4c81d445bf05e5d4b641a
Nils Heyman <nils.heyman@forefront.se>
2025-06-20 19:03:56 +0200

    edit harpoon to be global

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -2,6 +2,20 @@ return {
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" }
-    },
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("harpoon").setup({
+                settings = {
+                    save_on_toggle = true,
+                    save_on_change = true,
+                    mark_branch = false,
+
+                    -- Use a static key to make Harpoon global (not per-directory)
+                    key = function()
+                        return "global"
+                    end,
+                },
+            })
+        end,
+    }
 }
